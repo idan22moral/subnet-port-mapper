7936f9c944e496aa92a2219168450d18c1da60d9
Idan Moral <idan22moral@gmail.com>
2020-12-15 18:14:08 +0200

    code refactor

--- subnet_port_mapper.py
+++ subnet_port_mapper.py
@@ -14,22 +14,24 @@ def get_ports_by_ip(ip: str):
 
     if len(ports_data) > 0:
         for line in ports_data:
-            port, rest = line.split('/')[:2]
-            rest = rest.split(' ')
-            over = rest[0]
-            protocol = rest[-1]
-            ports[port] = {'type': over, 'protocol': protocol}
+            port, _ = line.split('/')[:2]
+            _ = _.split(' ')
+            protocol_type = _[0]
+            protocol = _[-1]
+            ports[port] = {'type': protocol_type, 'protocol': protocol}
         return ports
     return None
 
 
 def get_open_ports_in_subnet():
     hosts = {}
+
     # get information about the addresses in the subnet using the ARP tool
     raw_arp_data = re.split(
         r'\s+\n?', sp.getoutput('arp -a').split('Type')[1])[1:-1]
     zipped_arp_data = list(
         zip(raw_arp_data[::3], raw_arp_data[1::3], raw_arp_data[2::3]))
+
     # remove all the static IPs in the subnet
     filtered_arp_data = filter(
         lambda x: x if x[2] == 'dynamic' else None, zipped_arp_data)
